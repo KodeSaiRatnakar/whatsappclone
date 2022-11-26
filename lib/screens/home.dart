e95d8b91c556e115f4471f5e451824c4d69f0b7c
Sai Ratnakar <sairatnakar2000@gmail.com>
2022-11-26 09:48:47 +0530

    added tab view

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,38 +6,78 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ThemeData themeData = Theme.of(context);
-    return Scaffold(
-      backgroundColor: themeData.backgroundColor,
-      appBar: AppBar(
-        backgroundColor: themeData.primaryColor.withOpacity(0.1),
-        title: Text(
-          'WhatsApp',
-          style: TextStyle(
-            color: themeData.primaryColor.withOpacity(0.5),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          backgroundColor: themeData.backgroundColor,
+          appBar: AppBar(
+            backgroundColor: themeData.primaryColor.withOpacity(0.1),
+            title: Text(
+              'WhatsApp',
+              style: TextStyle(
+                color: themeData.primaryColor.withOpacity(0.5),
+              ),
+            ),
+            actions: [
+              appBarActions(
+                Icons.camera_alt_outlined,
+                () {},
+                themeData.primaryColor,
+              ),
+              appBarActions(
+                Icons.search,
+                () {},
+                themeData.primaryColor,
+              ),
+              appBarActions(
+                Icons.more_vert,
+                () {},
+                themeData.primaryColor,
+              ),
+            ],
+            bottom: TabBar(
+              tabs: [
+                tabBarText('CHATS', themeData.primaryColor),
+                tabBarText('STATUS', themeData.primaryColor),
+                tabBarText('CALLS', themeData.primaryColor)
+              ],
+            ),
           ),
-        ),
-        actions: [
-          appBarActions(
-            Icons.camera_alt_outlined,
-            () {},
-            themeData.primaryColor,
-          ),
-          appBarActions(
-            Icons.search,
-            () {},
-            themeData.primaryColor,
-          ),
-          appBarActions(
-            Icons.more_vert,
-            () {},
-            themeData.primaryColor,
-          ),
-        ],
-      ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: []),
+          body: TabBarView(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Text(
+                      "chats",
+                      style: TextStyle(color: themeData.primaryColor),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Center(
+                    child: Text(
+                      "status",
+                      style: TextStyle(color: themeData.primaryColor),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Center(
+                    child: Text(
+                      "calls",
+                      style: TextStyle(color: themeData.primaryColor),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )),
     );
   }
 }
@@ -48,6 +88,18 @@ Widget appBarActions(IconData icon, Function function, Color color) {
     icon: Icon(
       icon,
       color: color.withOpacity(0.7),
+    ),
+  );
+}
+
+Widget tabBarText(String text, Color color) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8.0),
+    child: Text(
+      text,
+      style: TextStyle(
+        color: color.withOpacity(0.5),
+      ),
     ),
   );
 }
